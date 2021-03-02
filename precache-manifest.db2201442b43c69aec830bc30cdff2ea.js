dc46360a28fca9642d455f1069d94dee534ca627
anuvarghese1395 <anuvarghese1395@gmail.com>
2021-03-02 22:07:18 +0100

    Updates

---
+++ precache-manifest.db2201442b43c69aec830bc30cdff2ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8ef9c5dda778254a140d6d21bdfbbd",
+    "url": "/camera-challenge-react/index.html"
+  },
+  {
+    "revision": "858035f8a5aef5e26dd2",
+    "url": "/camera-challenge-react/static/css/main.4c85ce82.chunk.css"
+  },
+  {
+    "revision": "724834b0e38d56a6bf3d",
+    "url": "/camera-challenge-react/static/js/2.fbaf9443.chunk.js"
+  },
+  {
+    "revision": "127c34995248f837456ddaca21e714fd",
+    "url": "/camera-challenge-react/static/js/2.fbaf9443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858035f8a5aef5e26dd2",
+    "url": "/camera-challenge-react/static/js/main.3499a3a2.chunk.js"
+  },
+  {
+    "revision": "e166944e0c9534a0e8cd",
+    "url": "/camera-challenge-react/static/js/runtime-main.d7a21b86.js"
+  }
+]);
